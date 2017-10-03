449fd5b37dbf788c87faea2a17b5f17b504f7a93
kaaSLDN <kaapl@gmail.com>
2017-10-03 09:28:09 +0300

    create catalog controller

---
+++ controllers/CatalogController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andri
+ * Date: 03.10.17
+ * Time: 9:25
+ */
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+
+class CatalogController extends Controller
+{
+    public function actionIndex()
+    {
+
+    }
+
+    public function actionCategory()
+    {
+
+    }
+
+    public function actionTag()
+    {
+
+    }
+
+    public function actionView()
+    {
+
+    }
+}
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -39,6 +39,7 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
+            ['label' => 'Catalog', 'url' => ['/site/catalog']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             ['label' => 'Admin', 'items' => [
